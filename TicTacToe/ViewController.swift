892ce4123b18799fbaefc5274a47a65dddbca44d
mansah82 <manne.sahlin@iths.se>
2021-11-23 15:39:34 +0100

    fixed a few things

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -12,8 +12,10 @@ class ViewController: UIViewController {
 let circleImage = "circleImage"
 let xImage = "xImage"
 var wichPlayer = true
+var counter = 0
 
-
+    @IBOutlet weak var playerLabel: UILabel!
+    
     
     
     override func viewDidLoad() {
@@ -24,13 +26,23 @@ var wichPlayer = true
     @IBAction func tap(_ sender: UITapGestureRecognizer) {
     
     if let imageView = sender.view as? UIImageView{
-        if wichPlayer == true {
+        if wichPlayer == true && imageView.tag == 0 {
             imageView.image = UIImage(named: circleImage)
+            imageView.tag = 1
+            wichPlayer.toggle()
+            counter += 1
+            playerLabel.text = "Player 2 turn"
             
-        } else {
+        } else if wichPlayer == false && imageView.tag == 0{
             imageView.image = UIImage(named: xImage)
+            imageView.tag = 2
+            wichPlayer.toggle()
+            counter += 1
+            playerLabel.text = "Player 1 turn"
         }
-        wichPlayer.toggle()
+        if counter == 9{
+            playerLabel.text = "Your out of moves"
+        }
         }
     }
 

3d0f6874d90354d6cc11aec660bc1c77df366bd6
mansah82 <manne.sahlin@iths.se>
2021-11-23 15:15:12 +0100

    first draft

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+let circleImage = "circleImage"
+let xImage = "xImage"
+var wichPlayer = true
+
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+    
+    if let imageView = sender.view as? UIImageView{
+        if wichPlayer == true {
+            imageView.image = UIImage(named: circleImage)
+            
+        } else {
+            imageView.image = UIImage(named: xImage)
+        }
+        wichPlayer.toggle()
+        }
+    }
+
 
 }
+
+    
+    
+    
+
+    
+
 
